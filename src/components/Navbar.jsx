06c085b1fce5edd333e8e56034baca0a825acc72
shahan210 <mohammedshahan210@gmail.com>
2024-03-22 21:57:47 +0530

    chore : navbar complete

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { MdOutlineMenuOpen } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
+  return (
+    <div className="text-white flex justify-center px-4 mx-auto h-24 max-w-[1240px] items-center">
+      <h1 className=" m-4 w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+      <ul className="md:flex hidden">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
+      <div onClick={handleNav} className=" block md:hidden">
+        {!nav ? <MdOutlineMenuOpen size={20} /> : <IoClose size={20} />}
+      </div>
+      <div
+        className={
+          nav
+            ? "fixed h-full left-0 top-0 w-[60%] border-r bg-[#000300] border-r-gray-900 ease-in-out duration-500"
+            : "fixed left-[-100%] "
+        }
+      >
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">REACT.</h1>
+        <ul className="pt-24 uppercase">
+          <li className="p-4 border-b border-b-gray-600">Home</li>
+          <li className="p-4 border-b border-b-gray-600">Company</li>
+          <li className="p-4 border-b border-b-gray-600">Resources</li>
+          <li className="p-4 border-b border-b-gray-600">About</li>
+          <li className="p-4">Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
